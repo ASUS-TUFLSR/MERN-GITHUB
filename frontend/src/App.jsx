ea1de4d3096337412e554b9e92fee725a2142208
Manmit Pal <manmitpal348@gmail.com>
2025-10-30 13:02:05 +0530

    Downloaded svg's and Created Routes And Pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import {Route ,Router} from "react-router-dom"
+import HomePage from "./pages/HomePage"
+import LoginPage from "./pages/LoginPage"
+import SignUpPage from "./pages/SignUpPage"
+import ExplorePage from "./pages/ExplorePage"
+import LikesPage from "./pages/LikesPage"
+
 
 const App = () => {
   return (
-    <div className='text-2xl'>App</div>
+    <div className='flex'>
+      <Sidebar/>
+      <div>
+        <Router>
+            <Route path={"/"} Component={<HomePage/>} />
+            <Route path={"/login"} Component={<LoginPage/>} />
+            <Route path={"/signup"} Component={<SignUpPage/>} />
+            <Route path={"/explore"} Component={<ExplorePage/>} />
+            <Route path={"/likes"} Component={<LikesPage/>} />
+        </Router>
+      </div>
+    </div>
   )
 }
 
