688432b35ecf08dc9dead4f619ec1aa606c3f35e
Manmit Pal <manmitpal348@gmail.com>
2025-11-12 18:07:40 +0530

    .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,8 @@ import SignUpPage from "./pages/SignUpPage"
 import ExplorePage from "./pages/ExplorePage"
 import LikesPage from "./pages/LikesPage"
 import Sidebar from './components/Sidebar'
+import {Toaster} from "react-hot-toast"
+
 
 const App = () => {
   return (
@@ -19,6 +21,7 @@ const App = () => {
             <Route path={"/explore"} element={<ExplorePage/>} />
             <Route path={"/likes"} element={<LikesPage/>} />
         </Routes>
+        <Toaster/>
       </div>
     </div>
   )
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Search from '../components/Search'
 import SortRepos from '../components/SortRepos'
 import ProfileInfo from '../components/ProfileInfo'
@@ -7,6 +7,25 @@ import Spinner from '../components/Spinner'
 
 
 const HomePage = () => {
+
+  const [userProfile, setUserProfile] = useState(null);
+  const [repos, setRepos] = useState([]);
+  const [loading, setLoading] = (false);
+
+  const [sortType, setSortType] = useState("forks")
+
+  const getUserProfileAndRepos = async () => {
+      try {
+        // Fetch USer Profile And Repos Using Github API
+      } catch (error) {
+        toast.error(error.message)
+      }
+  }
+
+  useEffect(() => {
+    getUserProfileAndRepos();
+  }, [])
+
   return (
     <div className='m-4' >
       <Search/>
