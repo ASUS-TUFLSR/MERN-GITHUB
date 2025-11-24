96e812a6b9d858616d9a4a9a5daa4e81cabcb42c
Manmit Pal <manmitpal348@gmail.com>
2025-11-24 12:33:15 +0530

    Create Controller and Route for user, succesfully fetching user deatils in backend

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,24 @@
+import express from "express";
+
+export const getUserProfileAndRepos = async (req,res) => {
+    const {username} = req.params;
+       try {
+         const userRes = await fetch(`https://api.github.com/users/${username}`,{
+           headers: {
+            Authorization: `token ${process.env.GITHUB_API_KEY}`
+           },
+        });
+        const userProfile = await userRes.json();
+        
+        const reposRes = await fetch(userProfile.repos_url, {
+           headers: {
+            Authorization: `token ${process.env.GITHUB_API_KEY}`
+           }, 
+        });
+        const repos = await reposRes.json();
+
+        res.status(200).json({userProfile, repos})
+       } catch (error) {
+        res.status(500).json({error: error.message})
+       }
+}
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,10 @@
 import express from "express";
+import dotenv from 'dotenv';
 
+import userRoutes from "./routes/user.router.js"
+
+
+dotenv.config();
 
 const app = express();
 const PORT = 5000;
@@ -8,6 +13,8 @@ const PORT = 5000;
 app.get("/", (req, res) => {
     res.send("Server Is Live");
 });
+
+app.use("/api/users", userRoutes)
 
 app.listen(PORT, () => {
     console.log(`Server running on Port ${PORT}`)
