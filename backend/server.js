6c9e7abedbddd572abbde348ae353529dea21238
Manmit Pal <manmitpal348@gmail.com>
2025-11-27 13:14:03 +0530

    Succesfully LoggedIn The User, will integrate it with our client side now

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,33 @@
 import express from "express";
 import dotenv from 'dotenv';
 import cors from "cors"
+import "./passport/github.auth.js"
+import passport from "passport";
+import session from "express-session";
 import userRoutes from "./routes/user.router.js"
 import exploreRoutes from "./routes/explore.router.js"
+import authRoutes from "./routes/auth.router.js"
 import connectMongoDB from "./db/db.js";
+
 
 dotenv.config();
 
 const app = express();
 const PORT = 5000;
+
+app.use(session({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
+// Initialize Passport!  Also use passport.session() middleware, to support
+// persistent login sessions (recommended).
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(cors());
 
 app.get("/", (req, res) => {
     res.send("Server Is Live");
 });
 
+app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes)
 app.use("/api/explore", exploreRoutes)
 
