0299fbc6a21a27e5fbff6ec632103124db24f923
Manmit Pal <manmitpal348@gmail.com>
2025-11-28 20:28:24 +0530

    Created endpoints for our likesPage in backend

--- backend/routes/user.router.js
+++ backend/routes/user.router.js
@@ -1,12 +1,13 @@
 import express from "express";
-import { getUserProfileAndRepos } from "../controllers/user.controller.js";
+import { getLikes, getUserProfileAndRepos, likeProfile } from "../controllers/user.controller.js";
+import { ensureAuthenticated } from "../middleware/ensureAuthenticated.js";
 
 const router = express.Router();
 
 router.get("/profile/:username", getUserProfileAndRepos);
-// TODO => Get likes (which/who liked our profile)
-// TODO => POST like a profile
 
+router.get("/likes", ensureAuthenticated, getLikes)
+router.post("/like/:username", ensureAuthenticated, likeProfile)
 
 
 export default router;
