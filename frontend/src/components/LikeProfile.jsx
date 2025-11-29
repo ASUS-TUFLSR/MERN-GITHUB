815bff07fcf7a7630d52966d7271cbc7f271e3ba
Manmit Pal <manmitpal348@gmail.com>
2025-11-29 12:28:25 +0530

    User Like Fn() working

--- frontend/src/components/LikeProfile.jsx
+++ frontend/src/components/LikeProfile.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
 import { FaHeart } from 'react-icons/fa6'
+import toast from "react-hot-toast"
+import {useAuthContext} from "../context/AuthContext"
 
-const LikeProfile = () => {
+const LikeProfile = ({userProfile}) => {
 
+  const {authUser} = useAuthContext();
+  const isOwnProfile = authUser?.username === userProfile.login;
   const handleLikeProfile = async () => {
-       // sennd request to our endpoint
+       try {
+        const res = await fetch(`/api/users/like/${userProfile.login}`, {
+          method: "POST",
+          credentials: "include",
+        });
+        const data = await res.json();
+        if(data.error) throw new Error(data.error);
+        toast.success(data.message)
+       } catch (error) {
+        toast.error(error.message)
+       }
   }
 
+  if(!authUser || isOwnProfile) return null;
+ 
     return (
     <button
 			className='p-2 text-xs w-full font-medium rounded-md bg-glass border border-blue-400 flex items-center gap-2'
